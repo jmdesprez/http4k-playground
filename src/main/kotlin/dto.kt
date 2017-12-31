9a1bd9bbffb797313e99613c8d7a6cd4b4acea40
Desprez Jean-Marc <jmdesprez@protonmail.com>
2017-12-31 01:32:22 +0100

    Add lens for user

--- src/main/kotlin/dto.kt
+++ src/main/kotlin/dto.kt
@@ -4,3 +4,6 @@ enum class Role {
 
 data class UserDTO(val login: String, val role: Role)
 
+val userRoleLens = Lens(UserDTO::role, { newValue -> { user -> user.copy(role = newValue) } })
+
+val userLoginLens = Lens(UserDTO::login, { newValue -> { user -> user.copy(login = newValue) } })
